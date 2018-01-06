7bcf5c32f37322a5cc16bdb4e12fb61a63a13b64
xxhomey19 <xxhomey19@gmail.com>
2018-01-07 01:19:06 +0800

    Fix background error while running tabs.executeScript

--- src/js/__tests__/background.spec.js
+++ src/js/__tests__/background.spec.js
@@ -1,12 +1,13 @@
 describe('background', () => {
   let background;
-  beforeEach(() => {
-    jest.resetModules();
+  const setup = (url = 'https://github.com/xxhomey19/github-file-icon') => {
     global.chrome = {
       webNavigation: {
         onHistoryStateUpdated: {
           addListener: jest.fn(fn => {
-            fn();
+            fn({
+              url,
+            });
           }),
         },
       },
@@ -16,16 +17,26 @@ describe('background', () => {
     };
     // eslint-disable-next-line global-require
     background = require('../background');
+  };
+  beforeEach(() => {
+    jest.resetModules();
   });
 
   it('should exist', () => {
+    setup();
     expect(background).toBeDefined();
   });
 
   it('should call addListener and executeScript', () => {
+    setup();
     expect(chrome.webNavigation.onHistoryStateUpdated.addListener).toBeCalled();
     expect(chrome.tabs.executeScript).toBeCalledWith(null, {
       file: 'contentscript.bundle.js',
     });
+  });
+
+  it('should not executeScript if url is not GitHub', () => {
+    setup('https://www.google.com.tw');
+    expect(chrome.tabs.executeScript).not.toBeCalled();
   });
 });
