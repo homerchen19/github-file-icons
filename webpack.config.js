741bbeafc81a6034d2ac2381b27abce9ca422cd2
xxhomey19 <xxhomey19@gmail.com>
2018-01-21 10:14:11 +0800

    Adjust build script

--- webpack.config.js
+++ webpack.config.js
@@ -95,6 +95,20 @@ const options = {
 
 if (env.NODE_ENV === 'development') {
   options.devtool = 'cheap-module-eval-source-map';
+} else if (env.NODE_ENV === 'production') {
+  options.plugins.push(
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false,
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        screw_ie8: true,
+        warnings: false,
+      },
+      sourceMap: true,
+    })
+  );
 }
 
 module.exports = options;
