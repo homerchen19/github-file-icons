76a265cc8218c6678f1aa403f6571405b644640c
xxhomey19 <xxhomey19@gmail.com>
2018-02-22 17:11:33 +0800

    Refactor contetnscript.js

--- src/js/contentscript.js
+++ src/js/contentscript.js
@@ -5,106 +5,64 @@ import '../css/icons.css';
 const select = document.querySelector.bind(document);
 select.all = document.querySelectorAll.bind(document);
 
+const getSelector = () => {
+  switch (window.location.hostname) {
+    case 'github.com':
+      return {
+        filenameSelector: 'tr.js-navigation-item > td.content > span > a',
+        iconSelector: 'tr.js-navigation-item > td.icon',
+        host: 'github',
+      };
+    case 'gitlab.com':
+      return {
+        filenameSelector: 'tr.tree-item > td.tree-item-file-name > a > span',
+        iconSelector: 'tr.tree-item > td.tree-item-file-name > i',
+        host: 'gitlab',
+      };
+    case 'bitbucket.org':
+      return {
+        filenameSelector: 'tr.iterable-item > td.filename > div > a',
+        iconSelector: 'tr.iterable-item > td.filename > div > a > span',
+        host: 'bitbucket',
+      };
+    default:
+      return {
+        filenameSelector: 'tr > td.name > a',
+        iconSelector: 'tr > td.name > span',
+        host: 'others',
+      };
+  }
+};
+
 const update = () => {
-  const filenameDoms = Array.from(
-    select.all('tr.js-navigation-item > td.content > span > a')
-  );
-  const iconDoms = Array.from(select.all('tr.js-navigation-item > td.icon'));
+  const { filenameSelector, iconSelector, host } = getSelector();
+
+  const filenameDoms = Array.from(select.all(filenameSelector));
+  const iconDoms = Array.from(select.all(iconSelector));
 
   const filenameDomsLength = filenameDoms.length;
 
   if (filenameDomsLength !== 0) {
     for (let i = 0; i < filenameDomsLength; i += 1) {
       const { innerText: filename } = filenameDoms[i];
-      const iconDom = iconDoms[i];
+      const iconDom =
+        host === 'github' ? iconDoms[i].querySelector('.octicon') : iconDoms[i];
 
-      const oldIcon = iconDom.querySelector('.octicon');
-      const isDirectory = oldIcon.classList.contains('octicon-file-directory');
-
-      const className = fileIcons.getClassWithColor(filename);
-
-      if (className && !isDirectory) {
-        iconDom.innerHTML = `<span class='icon ${className}'></span>`;
-      }
-    }
-  }
-
-  const filenameDomsGitlab = Array.from(
-    select.all('tr.tree-item > td.tree-item-file-name > a > span')
-  );
-
-  const iconDomsGitlab = Array.from(
-    select.all('tr.tree-item > td.tree-item-file-name > i')
-  );
-
-  const filenameDomsGitlabLength = filenameDomsGitlab.length;
-
-  if (filenameDomsGitlabLength !== 0) {
-    for (let i = 0; i < filenameDomsGitlabLength; i += 1) {
-      const { innerText: filename } = filenameDomsGitlab[i];
-      const iconDomGitlab = iconDomsGitlab[i];
-
-      const isDirectory = iconDomGitlab.classList.contains('fa-folder');
+      const isDirectory =
+        iconDom.classList.contains('octicon-file-directory') ||
+        iconDom.classList.contains('fa-folder');
 
       const className = fileIcons.getClassWithColor(filename);
 
       if (className && !isDirectory) {
-        const icon = document.createElement('span');
-        icon.className = `${className}`;
-        iconDomGitlab.parentNode.replaceChild(icon, iconDomGitlab);
-      }
-    }
-  }
-
-  const filenameDomsBitbucket = Array.from(
-    select.all('tr.iterable-item > td.filename > div > a')
-  );
-
-  const iconDomsBitbucket = Array.from(
-    select.all('tr.iterable-item > td.filename > div > a > span')
-  );
-
-  const filenameDomsBitbucketLength = filenameDomsBitbucket.length;
-
-  if (filenameDomsBitbucketLength !== 0) {
-    for (let i = 0; i < filenameDomsBitbucketLength; i += 1) {
-      const { innerText: filename } = filenameDomsBitbucket[i];
-      const iconDomBitbucket = iconDomsBitbucket[i];
-
-      const className = fileIcons.getClassWithColor(filename);
-
-      if (className) {
-        const icon = document.createElement('span');
-        icon.className = `${className}`;
-        icon.style.marginRight = '10px';
-        iconDomBitbucket.parentNode.replaceChild(icon, iconDomBitbucket);
-      }
-    }
-  }
-
-  const filenameDomsGogsGitea = Array.from(select.all('tr > td.name > a'));
-
-  const iconDomsGogsGitea = Array.from(select.all('tr > td.name > span'));
-
-  const filenameDomsGogsGiteaLength = filenameDomsGogsGitea.length;
-
-  if (filenameDomsGogsGiteaLength !== 0) {
-    for (let i = 0; i < filenameDomsGogsGiteaLength; i += 1) {
-      const { innerText: filename } = filenameDomsGogsGitea[i];
-      const iconDomGogsGitea = iconDomsGogsGitea[i];
-
-      // const oldIcon = iconDomGogsGitea.querySelector('.octicon');
-      const isDirectory = iconDomGogsGitea.classList.contains(
-        'octicon-file-directory'
-      );
-
-      const className = fileIcons.getClassWithColor(filename);
-
-      if (className && !isDirectory) {
-        const icon = document.createElement('span');
-        icon.className = `${className}`;
-        icon.style.marginRight = '10px';
-        iconDomGogsGitea.parentNode.replaceChild(icon, iconDomGogsGitea);
+        if (host === 'github') {
+          iconDoms[i].innerHTML = `<span class='icon ${className}'></span>`;
+        } else {
+          const icon = document.createElement('span');
+          icon.className = `${className}`;
+          icon.style.marginRight = host === 'bitbucket' ? '10px' : '3px';
+          iconDoms[i].parentNode.replaceChild(icon, iconDoms[i]);
+        }
       }
     }
   }
