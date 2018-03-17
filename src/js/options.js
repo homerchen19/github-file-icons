755cad9d343547b13d02210c1490a2949f25df06
Arnelle Balane <arnellebalane@gmail.com>
2018-03-17 11:55:42 +0800

    Display save status messages

--- src/js/options.js
+++ src/js/options.js
@@ -1,14 +1,31 @@
 import '../css/options.css';
 
-const form = document.querySelector('form');
+const $form = document.querySelector('form');
+const $message = document.querySelector('.message');
+
+function showMessage(message, duration) {
+  $message.textContent = message;
+  if (duration) {
+    setTimeout(() => {
+      $message.textContent = '';
+    }, duration);
+  }
+}
 
 chrome.storage.sync.get('colorsEnabled', result => {
-  form.colors.checked = Boolean(result.colorsEnabled);
+  $form.colors.checked = Boolean(result.colorsEnabled);
 });
 
-form.addEventListener('submit', e => {
+$form.addEventListener('submit', e => {
   e.preventDefault();
 
-  const colorsEnabled = form.colors.checked;
-  chrome.storage.sync.set({ colorsEnabled });
+  const colorsEnabled = $form.colors.checked;
+
+  showMessage('Saving...');
+  $form.save.disabled = true;
+
+  chrome.storage.sync.set({ colorsEnabled }, () => {
+    showMessage('Saved!', 1000);
+    $form.save.disabled = false;
+  });
 });
