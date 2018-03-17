895773d8a8cb045a0316452ecbbdadcae28746d3
Arnelle Balane <arnellebalane@gmail.com>
2018-03-17 11:37:48 +0800

    Implement saving of user options in storage.sync

--- src/js/options.js
+++ src/js/options.js
@@ -1,1 +1,14 @@
 import '../css/options.css';
+
+const form = document.querySelector('form');
+
+chrome.storage.sync.get('colorsEnabled', result => {
+  form.colors.checked = Boolean(result.colorsEnabled);
+});
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const colorsEnabled = form.colors.checked;
+  chrome.storage.sync.set({ colorsEnabled });
+});
