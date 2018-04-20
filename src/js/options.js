99f3c5e22d185e1bde6e6bd454d0804c76a5309d
xxhomey19 <xxhomey19@gmail.com>
2018-04-20 22:13:07 +0800

    Add dark mode option

--- src/js/options.js
+++ src/js/options.js
@@ -5,6 +5,7 @@ const $message = document.querySelector('.message');
 
 function showMessage(message, duration) {
   $message.textContent = message;
+
   if (duration) {
     setTimeout(() => {
       $message.textContent = '';
@@ -12,19 +13,21 @@ function showMessage(message, duration) {
   }
 }
 
-chrome.storage.sync.get('colorsDisabled', result => {
-  $form.colors.checked = !(result || {}).colorsDisabled;
+chrome.storage.sync.get(['colorsEnabled', 'darkMode'], result => {
+  $form.colors.checked = result.colorsEnabled || true;
+  $form.darkMode.checked = result.darkMode || false;
 });
 
 $form.addEventListener('submit', e => {
   e.preventDefault();
 
-  const colorsDisabled = !$form.colors.checked;
+  const colorsEnabled = $form.colors.checked;
+  const darkMode = $form.darkMode.checked;
 
   showMessage('Saving...');
   $form.save.disabled = true;
 
-  chrome.storage.sync.set({ colorsDisabled }, () => {
+  chrome.storage.sync.set({ colorsEnabled, darkMode }, () => {
     showMessage('Saved!', 1000);
     $form.save.disabled = false;
   });
