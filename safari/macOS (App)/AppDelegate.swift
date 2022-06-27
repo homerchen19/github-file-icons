96165ae668c063538eae3ac384433c8676c552a3
Christopher Pickering <christopher.pickering@keemail.me>
2022-06-27 08:49:44 -0500

    updated

--- safari/macOS (App)/AppDelegate.swift
+++ safari/macOS (App)/AppDelegate.swift
@@ -9,13 +9,7 @@ import Cocoa
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-    func applicationDidFinishLaunching(_ notification: Notification) {
-        // Override point for customization after application launch.
-    }
-
     func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
         return true
     }
-
 }
